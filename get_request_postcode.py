afca10284a6d40cea99445f9ce45ad2f2f892e6f
khanmaster <eashkhan@hotmail.com>
2020-06-03 10:48:50 +0100

    python requests package use case

---
+++ get_request_postcode.py
@@ -0,0 +1,30 @@
+
+import requests
+
+path = 'http://api.postcodes.io/postcodes/'
+arguments = 'e147le'
+
+url_target = path + arguments
+print(url_target)
+
+# Make request and capture response
+response = requests.get(url_target)
+
+print(response)
+print(type(response))
+
+# Parsing or getting the dictionary out
+print(response.json())
+response_dictionary = response.json()
+
+print(type(response_dictionary))
+
+# for key in response_dictionary.keys():
+#     print(key)
+
+result_dictionary = response_dictionary['result']
+
+print(result_dictionary)
+
+for key in result_dictionary.keys():
+    print(key, 'the value inside is', result_dictionary[key])
---
+++ python_packages.py
@@ -0,0 +1,47 @@
+# install python package requests
+# on command line run - pip install requests
+# verify it with - pip -V
+
+import requests
+
+response_bbc = requests.get("http://www.bbc.co.uk/")
+
+print(response_bbc.status_code)
+#print(response_bbc.headers)
+# print(response_bbc.content)
+
+# first iteration
+if response_bbc.status_code == 200:
+    print("Response successful")
+elif response_bbc.status_code == 404:
+    print(" Sorry page not found")
+else:
+    print(" Opps... something went wrong")
+
+# second iteration to simplify our code and make it reusable
+#
+# def check_response_code():
+#     if response_bbc.status_code == 200:
+#         print("Response successful")
+#     elif response_bbc.status_code == 404:
+#         print(" Sorry page not found")
+#     else:
+#         print(" Opps... something went wrong")
+# print(check_response_code())
+#
+# # 3rd iteration to apply best practice
+# # def check_response_code():
+#
+# #     if response_bbc:
+# #         print("Response successful")
+#     elif response_bbc:
+#         print(" Sorry page not found")
+#     else:
+#         print(" Opps some went wrong")
+#
+# print(check_response_code())
+
+# requests goes one step further in simplifying this process for us.
+# If you use a response_bcc instance in a conditional expression,
+# It will evaluate to True if the status code was between 200 and 400, and False otherwise.
+# Therefore, you can simplify the last example by rewriting the if statement as above:
